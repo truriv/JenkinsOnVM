ea1ae676483a4334812ffe3e4a87749bb29d975e
Ubuntu <sinil@vm001.3jeohfjyrs4eba0bd1mellggzg.syx.internal.cloudapp.net>
2020-06-18 08:08:30 +0000

    Third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Hello world!!!');
+    res.send('Hello world!!!!!');
 });
 
 app.get('/start', function(req, res){
